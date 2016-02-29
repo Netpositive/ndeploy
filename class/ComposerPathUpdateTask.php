bf50f52dc936c576cb652b5a4480ac3b8df24907
Peter Buri <peter.buri@netpositive.hu>
2016-02-29 08:55:21 +0100

    Added composer.lock path updater for monolithic repositories.

--- class/ComposerPathUpdateTask.php
+++ class/ComposerPathUpdateTask.php
@@ -15,6 +15,7 @@
 class ComposerPathUpdateTask extends Task
 {
     const COMPOSER_JSON = 'composer.json';
+    const COMPOSER_JSON_LOCK = 'composer.lock';
     const COMPOSER_REPOSITORY_TYPE_PATH = 'path';
 
     /**
@@ -38,8 +39,10 @@ class ComposerPathUpdateTask extends Task
 
     public function main()
     {
-        // Find the composer.json
+        // Find the composer.json and composer.lock
         $composerFile = implode('/', array($this->releasedir, self::COMPOSER_JSON));
+        $composerLockFile = implode('/', array($this->releasedir, self::COMPOSER_JSON_LOCK));
+
         if (!file_exists($composerFile)) {
             throw new BuildException('Composer JSON not found: ' . $composerFile);
         }
@@ -53,6 +56,7 @@ class ComposerPathUpdateTask extends Task
                 if (isset($repository['type'])
                     && self::COMPOSER_REPOSITORY_TYPE_PATH == strtolower($repository['type'])
                     && isset($repository['url'])
+                    && false !== strpos($repository['url'], '..')
                 ) {
                     $url = realpath(implode('/', array($this->getRepositorydir(), $repository['url'])));
                     if (!$url) {
@@ -76,6 +80,50 @@ class ComposerPathUpdateTask extends Task
             if ($changed) {
                 file_put_contents($composerFile, json_encode($composerConfig, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
             }
+        }
+
+        if (!file_exists($composerLockFile)) {
+            return;
+        }
+
+        // Read composer.lock file content
+        $composerLockConfig = json_decode(file_get_contents($composerLockFile), true);
+
+        if (isset($composerLockConfig['packages']) && count($composerLockConfig['packages'])) {
+            $changed = false;
+
+            foreach ($composerLockConfig['packages'] as $key => $package) {
+
+                if (isset($package['dist']['type'])
+                    && self::COMPOSER_REPOSITORY_TYPE_PATH == strtolower($package['dist']['type'])
+                    && isset($package['dist']['url'])
+                    && false !== strpos($package['dist']['url'], '..')
+                ) {
+
+
+                    $url = realpath(implode('/', array($this->getRepositorydir(), $package['dist']['url'])));
+                    if (!$url) {
+                        throw new BuildException('Composer path repository URL not found: ' . $url);
+                    }
+
+                    // Set the new url
+                    $composerLockConfig['packages'][$key]['dist']['url'] = $url;
+
+                    // Set the symlink parameter
+                    if (!isset($composerLockConfig['packages'][$key]['transport-options'])) {
+                        $composerLockConfig['packages'][$key]['transport-options'] = array();
+                    }
+                    $composerLockConfig['packages'][$key]['transport-options']['symlink'] = $this->symlinkAllowed;
+
+                    $changed = true;
+                }
+            }
+
+            // If json file change we over write it
+            if ($changed) {
+                file_put_contents($composerLockFile, json_encode($composerLockConfig, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+            }
+
         }
     }
 
